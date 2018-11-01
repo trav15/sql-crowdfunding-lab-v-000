3efb9880d58001ac3527602c2bd788fef652b010
Travis T <travistokuyama@gmail.com>
2018-11-01 16:12:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,35 @@
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, 'Learn.co', 'website',)
-
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES 
+(1, 'Learn.co', 'website', 1000, '02-14-2017', '02-14-2018'),
+(2, 'Pancake maker', 'food', 300, '01-01-2018', '03-06-2018'),
+(3, 'Bottlecap Collector', 'drink', 900, '03-11-2018', '06-11-2018'),
+(4, 'Kitchen Mixer', 'food', 2000, '04-11-2018', '06-11-2018'),
+(5, 'Vinyl River', 'music', 3000, '03-1-2018', '06-11-2018'),
+(6, 'Cleaner De Lune', 'homegoods', 5000, '06-11-2018', '09-20-2018'),
+(7, 'Monstera Delicious', 'plants', 400, '02-27-2018', '06-12-2018'),
+(8, 'Trinket collectors anonymous', 'homegoods', 500, '04-11-2018', '09-09-2018'),
+(9, 'New drum set','music', 500, '02-02-2018', '06-11-2018'),
+(10, 'Ti Leaf, not Tea Leaf', 'plants', 200, '04-04-2018', '07-07-2018')
 
 INSERT INTO users (id, name, age) VALUES
-
+(1, 'John', 20),
+(2, 'Tom', 25),
+(3, 'Ron', 30),
+(4, 'Don', 28),
+(5, 'Jon', 60),
+(6, 'Han', 70),
+(7, 'Mon', 40),
+(8, 'Lonne', 45),
+(9, 'Fawn', 33),
+(10, 'Con', 66),
+(11, 'Blonde', 22),
+(12, 'Won', 55),
+(13, 'Quan', 18),
+(14, 'Eon', 23),
+(15, 'Tawn', 32),
+(16, 'Pawn', 20),
+(17, 'Xon', 19),
+(18, 'Ion', 22),
+(19, 'Gonn', 50),
+(20, 'Done', 18)
 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
