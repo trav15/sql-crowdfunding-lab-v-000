73e5b8e47db62a605d20299b519fecc01019940d
Travis T <travistokuyama@gmail.com>
2018-11-01 15:17:22 +0000

    automatically backed up by learn

---
+++ lib/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE projects (
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal INTEGER,
+start_date TEXT,
+end_date TEXT
+);
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER, 
+);
