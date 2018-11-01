df44789871284ab498ba3ae52bfd1d5c5290fcaa
Travis T <travistokuyama@gmail.com>
2018-11-01 15:23:55 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+
+
+INSERT INTO users (id, name, age) VALUES
+
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
