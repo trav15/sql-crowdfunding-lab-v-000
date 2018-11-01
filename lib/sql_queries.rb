8c08ce66255cceb625bacdf87c5cecc7e5dbe898
Travis T <travistokuyama@gmail.com>
2018-11-01 16:25:22 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,6 +26,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
     "SELECT projects.category, pledges.amount FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'music';"
 end
 
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-  "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books' GROUP BY projects.category;"
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
+    "SELECT projects.category, SUM(pledges.amount) FROM projects JOIN pledges ON projects.id = pledges.project_id WHERE projects.category = 'books' GROUP BY projects.category;"
 end
